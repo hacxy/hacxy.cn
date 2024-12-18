a6dc5b222d170c04c0918cb682f642076d28a592
hacxy <hacxy.js@outlook.com>
2024-12-18 22:59:12 +0800

    build: change config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -12,32 +12,30 @@ export default defineConfig({
     ignoreDeadLinks: true,
     themeConfig: {
         posts: await getPosts(pageSize),
-        website: 'https://github.com/airene/vitepress-blog-pure', //copyright link
+        website: 'https://github.com/hacxy', //copyright link
         // 评论的仓库地址
-        comment: {
-            repo: 'airene/vitepress-blog-pure',
-            themes: 'github-light',
-            issueTerm: 'pathname'
-        },
+        // comment: {
+        //     repo: 'hacxy/blog',
+        //     themes: 'github-light',
+        //     issueTerm: 'pathname'
+        // },
         nav: [
             { text: 'Home', link: '/' },
-            { text: 'Category', link: '/pages/category' },
+            // { text: 'Category', link: '/pages/category' },
             { text: 'Archives', link: '/pages/archives' },
-            { text: 'Tags', link: '/pages/tags' },
-            { text: 'About', link: '/pages/about' }
+            { text: 'Tags', link: '/pages/tags' }
+            // { text: 'About', link: '/pages/about' }
             // { text: 'Airene', link: 'http://airene.net' }  -- External link test
         ],
         search: {
             provider: 'local'
         },
-        //outline:[2,3],
         outline: {
             label: '文章摘要'
         },
-        socialLinks: [{ icon: 'github', link: 'https://github.com/airene/vitepress-blog-pure' }]
+        socialLinks: [{ icon: 'github', link: 'https://github.com/hacxy' }]
     } as any,
     srcExclude: ['README.md'], // exclude the README.md , needn't to compiler
-
     vite: {
         //build: { minify: false }
         // server: { port: 5000 }
